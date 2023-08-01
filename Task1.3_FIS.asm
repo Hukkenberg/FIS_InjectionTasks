69dd82f717ae07189b39aad25b78c4daa5efc44d
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2023-08-01 14:34:54 +0700

    Update Task1.3_FIS.asm

--- Task1.3_FIS.asm
+++ Task1.3_FIS.asm
@@ -1,19 +1,68 @@
 .386
-.model flat,stdcall
-option casemap:none
-include \masm32\include\windows.inc
-include \masm32\include\kernel32.inc
-includelib \masm32\lib\kernel32.lib
-include \masm32\include\user32.inc
-includelib \masm32\lib\user32.lib
+.model	flat, stdcall
+option	casemap :none
+
+include		windows.inc
+include		user32.inc
+include		kernel32.inc
+include		comdlg32.inc
+includelib	user32.lib
+includelib	kernel32.lib
+includelib	comdlg32.lib
+
+DlgProc			PROTO		:DWORD,:DWORD,:DWORD,:DWORD
+Validate		PROTO		:DWORD
+AddSection		PROTO		:DWORD
 
 .data
-MsgBoxCaption  db "Warning!",0
-MsgBoxText     db "You have been injected!",0
-file_name      db 'src_t1.3_fis.txt'
+;for opening files
+MsgBoxCaption   db          "Warning!",0
+MsgBoxText      db          "You have been injected!",0
+file_name       db          "src_t1.3_fis.txt"
+;for creating new sections
+strFilter		db			"Executable Files (*.exe, *.dll)",0,
+							"*.exe;*.dll",0,"All Files",0,"*.*",0,0 
+OpenError		db			"Unable to open target file",0
+MsgBoxCap	    db		    "Results",0
+Invalid			db			"This is not a valid PE file!!",0
+Success			db			"New section added successfully",0
+NoSpace			db			"No space for new Section Header",0
+Backup			db			".bak",0
+;*********VALUES REQUIRED TO CONSTRUCT NEW SECTION************* 
+NewSecName		dd			"WEN."
+NewSecSize		dd			100h
+NewSecChar		dd			0E0000060h
+KeepBkup		db			TRUE
+
+.data?
+ofn			OPENFILENAME	<>
+hInstance	HINSTANCE	?
+hTarget		dd		?
+hMapping	dd		?
+pMapping	dd		?
+TargetName	db		512 dup(?)
+pPEHeader	dd			?
+Buffer		db			512 dup (?)
+
+.const
+IDD_MAIN	equ		1001
+IDC_TARGET	equ		1003
+IDC_OPEN	equ		1004
+IDC_BROWSE	equ			1004
+IDC_GO 		equ		1005
+IDC_EXIT 	equ		1006
+ARIcon		equ		2001
+
 
 .code
 start:
+	;initiating
+	invoke	GetModuleHandle, NULL
+	mov	hInstance, eax
+	invoke	DialogBoxParam,hInstance,IDD_MAIN,0,addr DlgProc,0
+	invoke	ExitProcess, eax
+	
+	;open the file
 	file_opening PROC
 		mov eax, 5
 		mov ebx, file_name
@@ -31,77 +80,261 @@ start:
    		mov ecx, info
    		mov edx, 26
 	ret
-	get_kernel32 PROC
-		xor ecx, ecx                
-		mul ecx                    
-		mov eax, [fs:ecx + 0x030]   
-		mov eax, [eax + 0x00c]      
-		mov esi, [eax + 0x014]      
-		lodsd                       
-		xchg esi, eax				
-		lodsd                       
-		mov ebx, [eax + 0x10] 
-		ret  
-	get_address_of_name PROC
-		mov edx, [ebx + 0x3c]       
-		add edx, ebx				
-		mov edx, [edx + 0x78]       
-		add edx, ebx
-		mov esi, [edx + 0x20]       
-		add esi, ebx
-		xor ecx, ecx 
-	ret  
-	get_proc_address PROC
-		inc ecx                             
-		lodsd                               
-		add eax, ebx				
-		cmp dword [eax], 0x50746547         
-		jnz get_proc_address
-		cmp dword [eax + 0x4], 0x41636F72   
-		jnz get_proc_address
-		cmp dword [eax + 0x8], 0x65726464   
-		jnz get_proc_address
-	ret 
-	get_proc_address_func PROC
-		mov esi, [edx + 0x24]       
-		add esi, ebx                
-		mov cx, [esi + ecx * 2]     
-		dec ecx
-		mov esi, [edx + 0x1c]       
-		add esi, ebx                
-		mov edx, [esi + ecx * 4]   
-		add edx, ebx                
-		mov ebp, edx                
+	
+	;validate the file (if it is a PE file or not)
+	DlgProc proc hWin:DWORD,uMsg:DWORD,wParam:DWORD,lParam:DWORD
+		mov	eax,uMsg
+		.if	eax==WM_INITDIALOG
+			invoke	LoadIcon,hInstance,2001
+			invoke	SendMessage,hWin,WM_SETICON,1,eax
+		.elseif eax==WM_COMMAND
+			mov	eax,wParam
+			.if	eax==IDC_OPEN
+				mov ofn.lStructSize,SIZEOF ofn 
+				mov ofn.lpstrFilter,offset strFilter
+				mov ofn.lpstrFile,offset TargetName 
+				mov ofn.nMaxFile,512 
+				mov ofn.Flags,OFN_FILEMUSTEXIST+OFN_PATHMUSTEXIST+\
+						OFN_LONGNAMES+OFN_EXPLORER+OFN_HIDEREADONLY 
+				invoke GetOpenFileName,addr ofn
+				.if eax==TRUE
+					invoke SetDlgItemText,hWin,IDC_TARGET,addr TargetName
+					invoke RtlZeroMemory,addr TargetName,512
+				.endif
+		.elseif eax==IDC_GO
+			invoke GetDlgItemText,hWin,IDC_TARGET,addr TargetName,512
+			invoke lstrlen,addr TargetName
+			.if eax!=0
+				invoke Validate,addr TargetName
+			.endif
+		.elseif eax==IDC_EXIT
+			invoke SendMessage,hWin,WM_CLOSE,0,0
+		.endif
+		.elseif	eax==WM_CLOSE
+			invoke	EndDialog,hWin,0
+		.endif
+		xor	eax,eax
+		ret
+	DlgProc endp
+
+	Validate proc FileName:DWORD
+		invoke CreateFile,FileName,GENERIC_READ,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0 
+		.if eax!=INVALID_HANDLE_VALUE
+			mov hTarget,eax
+			invoke CreateFileMapping,eax,0,PAGE_READ,0,0,0 
+			mov hMapping,eax
+			invoke MapViewOfFile,eax,FILE_MAP_READ,0,0,0 
+			mov pMapping,eax 
+			.if [eax.IMAGE_DOS_HEADER.e_magic]==IMAGE_DOS_SIGNATURE
+				add eax,[eax.IMAGE_DOS_HEADER.e_lfanew]
+				.if [eax.IMAGE_NT_HEADERS.Signature]==IMAGE_NT_SIGNATURE
+					invoke MessageBox,0,addr Valid,addr MsgBoxCap,MB_ICONASTERISK
+			.	endif 
+			.else 
+				invoke MessageBox,0,addr Invalid,addr MsgBoxCap,MB_ICONASTERISK
+			.endif 
+			invoke UnmapViewOfFile,pMapping
+			invoke CloseHandle,hMapping
+			invoke CloseHandle,hTarget
+		.else
+			invoke MessageBox,0,addr OpenError,0,0
+		.endif
+		xor eax,eax
+		Ret
+	Validate EndP
+
+	;create a new section - the guaranteed way to inject the messagebox, code cave is done later
+	DlgProc proc hWin:DWORD,uMsg:DWORD,wParam:DWORD,lParam:DWORD
+	mov	eax,uMsg
+	.if	eax==WM_INITDIALOG
+		invoke	LoadIcon,hInstance,2001
+		invoke	SendMessage,hWin,WM_SETICON,1,eax
+	.elseif eax==WM_COMMAND
+		mov	eax,wParam
+		.if	eax==IDC_BROWSE
+			mov ofn.lStructSize,SIZEOF ofn 
+			mov ofn.lpstrFilter,offset strFilter
+			mov ofn.lpstrFile,offset TargetName 
+			mov ofn.nMaxFile,512 
+			mov ofn.Flags,OFN_FILEMUSTEXIST+OFN_PATHMUSTEXIST+\
+						OFN_LONGNAMES+OFN_EXPLORER+OFN_HIDEREADONLY 
+			invoke GetOpenFileName,addr ofn
+			.if eax==TRUE
+				invoke SetDlgItemText,hWin,IDC_TARGET,addr TargetName
+				invoke RtlZeroMemory,addr TargetName,512
+			.endif
+		.elseif eax==IDC_GO
+			invoke GetDlgItemText,hWin,IDC_TARGET,addr TargetName,512
+			invoke lstrlen,addr TargetName
+			.if eax!=0
+				invoke Validate,addr TargetName
+				.if eax==1
+					invoke AddSection,addr TargetName
+					.if eax==1
+						invoke MessageBox,hWin,addr Success,0,0
+					.elseif eax==-1
+						invoke MessageBox,hWin,addr NoSpace,0,0
+					.endif
+				.elseif eax==-1
+					invoke MessageBox,hWin,addr Invalid,0,0
+				.endif		
+			.endif
+		.elseif eax==IDC_EXIT
+			invoke SendMessage,hWin,WM_CLOSE,0,0
+		.endif
+	.elseif	eax==WM_CLOSE
+		invoke	EndDialog,hWin,0
+	.endif
+	xor	eax,eax
 	ret
-	get_load_library_a PROC
-		xor ecx, ecx                
-		push ecx                    
-		push 0x41797261             
-		push 0x7262694c             
-		push 0x64616f4c             
-		push esp
-		push ebx                    
-		call edx                    
-	ret
-	get_user_32 PROC
-		push 0x61616c6c                
-		sub word [esp + 0x2], 0x6161    
-		push 0x642e3233                  
-		push 0x72657355                  
-		push esp
-		call eax  
-	ret
-	get_message_box PROC
-		push 0x6141786f                 
-		sub [esp + 0x3], 0x61
-		push 0x42656761                 
-		push 0x7373654d	                
-		push esp
-		push eax                        
-		call ebp                        
-	ret                      
-	invoke MessageBox, NULL, addr MsgBoxText, addr MsgBoxCaption, MB_OK
-	invoke ExitProcess, NULL
+	DlgProc endp
+
+	Validate proc FileName:DWORD
+	LOCAL ReturnValue:DWORD
+	invoke CreateFile,FileName,GENERIC_READ,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0 
+	.if eax!=INVALID_HANDLE_VALUE
+		mov hTarget,eax
+		invoke CreateFileMapping,eax,0,PAGE_READONLY,0,0,0 
+		mov hMapping,eax
+		invoke MapViewOfFile,eax,FILE_MAP_READ,0,0,0 
+		mov pMapping,eax 
+		.if [eax.IMAGE_DOS_HEADER.e_magic]==IMAGE_DOS_SIGNATURE
+			add eax,[eax.IMAGE_DOS_HEADER.e_lfanew]
+			.if [eax.IMAGE_NT_HEADERS.Signature]==IMAGE_NT_SIGNATURE
+				mov ReturnValue,1
+				jmp @EXIT
+			.endif 
+		.else 
+			mov ReturnValue,-1
+			jmp @EXIT
+		.endif 
+	@EXIT:
+		invoke UnmapViewOfFile,pMapping
+		invoke CloseHandle,hMapping
+		invoke CloseHandle,hTarget
+		mov eax,ReturnValue
+		ret
+	.else
+		invoke MessageBox,0,addr OpenError,0,0
+		xor eax,eax
+		Ret
+	.endif
+	Validate EndP
+
+	AddSection proc FileName:DWORD 
+	LOCAL ReturnValue:DWORD
+	;*******MAKE BACKUP***************************************************
+	invoke lstrcpy,addr Buffer,FileName
+	invoke lstrcat,addr Buffer,addr Backup		
+	invoke CopyFile,FileName,addr Buffer,TRUE
+	;*******OPEN FILE MAP IN AND INCREASE SIZE****************************
+	invoke CreateFile,FileName,GENERIC_READ+GENERIC_WRITE,0,0,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0 
+	.if eax!=INVALID_HANDLE_VALUE
+		mov hTarget,eax
+		invoke GetFileSize,eax,0
+		add eax,NewSecSize
+		invoke CreateFileMapping,hTarget,0,PAGE_READWRITE,0,eax,0 
+		mov hMapping,eax
+		invoke MapViewOfFile,eax,FILE_MAP_ALL_ACCESS,0,0,0 
+		mov pMapping,eax 
+	;******CHECK ENOUGH ROOM FOR NEW ISH IN SECTION TABLE******************
+		add eax,[eax.IMAGE_DOS_HEADER.e_lfanew]	;pointer to PE header in eax
+		mov pPEHeader,eax
+		movzx esi,[eax.IMAGE_NT_HEADERS.FileHeader.NumberOfSections];no of sections in ecx
+		inc esi									;add 1
+		imul esi,esi,40							;calc size of all ISHs
+		add esi,0F8h							;add above to offset of sec table from PE header
+		mov ebx,pMapping
+		add esi,[ebx.IMAGE_DOS_HEADER.e_lfanew]	;add above to offset of PE header from beginning of file
+		.if esi<[eax.IMAGE_NT_HEADERS.OptionalHeader.SizeOfHeaders]	;cmp offset of end of new ISH to offset of first section
+	;*******IF SO FILL NEW ISH**********************************************
+			add esi,ebx							;make above offset into a pointer by adding base address
+			sub esi,40							
+			mov edi,esi							;pointer to start of new ISH in edi
+			sub esi,40							;pointer to start of last ISH in esi
+			mov eax,NewSecName
+			mov dword ptr [edi.IMAGE_SECTION_HEADER.Name1],eax	;write in Name1
+			mov eax,NewSecSize
+			mov [edi.IMAGE_SECTION_HEADER.Misc.VirtualSize],eax	;write in virtual size
+			xor edx,edx											;align raw size of new sec
+			mov ebx,pPEHeader									
+			mov ecx,[ebx.IMAGE_NT_HEADERS.OptionalHeader.FileAlignment]
+			div ecx				;divides file alignment by raw size, quotient into eax, remainder into edx
+			.if edx!=0			;if no remainder, ie already aligned then multiply back up								
+				inc eax			;if there is remainder, ie not aligned, then
+			.endif				;add 1 to quotient and multiply back up (rounds up to alignment value)
+			mul ecx
+			mov [edi.IMAGE_SECTION_HEADER.SizeOfRawData],eax	;write in raw size
+			mov eax,[esi.IMAGE_SECTION_HEADER.VirtualAddress]	;get RVA of last section
+			add eax,[esi.IMAGE_SECTION_HEADER.Misc.VirtualSize]	;add virt size of last sec to get RVA of new sec
+			xor edx,edx											;align RVA of new sec
+			mov ecx,[ebx.IMAGE_NT_HEADERS.OptionalHeader.SectionAlignment]
+			div ecx
+			.if edx!=0
+				inc eax
+			.endif
+			mul ecx
+			mov [edi.IMAGE_SECTION_HEADER.VirtualAddress],eax	;write in RVA of new sec
+			mov eax,[esi.IMAGE_SECTION_HEADER.PointerToRawData]	;get raw offset of last sec
+			add eax,[esi.IMAGE_SECTION_HEADER.SizeOfRawData]	;add raw size
+			xor edx,edx											;align raw offset
+			mov ecx,[ebx.IMAGE_NT_HEADERS.OptionalHeader.FileAlignment]
+			div ecx
+			.if edx!=0
+				inc eax
+			.endif
+			mul ecx
+			mov [edi.IMAGE_SECTION_HEADER.PointerToRawData],eax	;write in raw offset of new sec
+			mov edx,NewSecChar
+			mov [edi.IMAGE_SECTION_HEADER.Characteristics],edx	;write in new sec characteristics
+			.if edx!=0
+				inc eax
+			.endif
+			mul ecx
+			message equ invoke MessageBox, NULL, addr MsgBoxText, addr MsgBoxCaption, MB_OK
+			mov [edi.IMAGE_SECTION_HEADER.Characteristics + 0x00001000], message    ;call the MsgBox
+	;*******UPDATE PE HEADER***********************************************
+			mov eax,[edi.IMAGE_SECTION_HEADER.Misc.VirtualSize]	;get new sec virt size
+			xor edx,edx											;align to section alignment
+			mov ecx,[ebx.IMAGE_NT_HEADERS.OptionalHeader.SectionAlignment]
+			div ecx
+			.if edx!=0
+				inc eax
+			.endif
+			mul ecx
+			add eax,[edi.IMAGE_SECTION_HEADER.VirtualAddress]	;add RVA where new section begins in memory
+			mov [ebx.IMAGE_NT_HEADERS.OptionalHeader.SizeOfImage],eax	;write new size of image
+			inc [ebx.IMAGE_NT_HEADERS.FileHeader.NumberOfSections];add 1 to no of sections
+			mov ReturnValue,1
+			jmp @EXIT
+		.else
+	;*******IF NO SPACE THEN RESTORE FROM BACKUP AND DELETE BACKUP**********
+			invoke UnmapViewOfFile,pMapping
+			invoke CloseHandle,hMapping
+			invoke CloseHandle,hTarget
+			invoke DeleteFile,FileName
+			invoke CopyFile,addr Buffer,FileName,FALSE
+			invoke DeleteFile,addr Buffer
+			mov ReturnValue,-1
+			jmp @EXIT
+		.endif
+	@EXIT:
+	;*******DECIDE WHETHER TO KEEP BACKUP AFTER SUCCESS*********************
+		invoke UnmapViewOfFile,pMapping
+		invoke CloseHandle,hMapping
+		invoke CloseHandle,hTarget
+		.if KeepBkup==FALSE
+			invoke DeleteFile,addr Buffer
+		.endif
+		mov eax,ReturnValue
+		ret
+	.else
+		invoke MessageBox,0,addr OpenError,0,0
+		xor eax,eax
+		Ret
+	.endif
+	Ret
+	AddSection EndP
 end start  
 
-;credentials: https://blackcloud.me/Win32-shellcode-3/, https://left404.com/2011/01/04/converting-x86-assembly-from-masm-to-nasm-3/ (for translation to MASM and tuneups)
+;credentials: ARTEam Win32 Assembly for Crackers, Goppit
